40d4df84321f0907f3c4be73e6e8840ec7d3a02f
shashijie <87419590@qq.com>
2024-08-01 14:37:41 +0800

    20240801

--- src/main/java/com/shashijie/cn/controller/ArticleController.java
+++ src/main/java/com/shashijie/cn/controller/ArticleController.java
@@ -25,10 +25,18 @@ public class ArticleController {
     private List<Article> articleList = new ArrayList<>();
     
     {
-        articleList.add(new Article("医疗反腐绝非砍医护收入", "时事", "2023-09-05", "已发布"));
-        articleList.add(new Article("中国劈篮缘何一败涂地?", "篮球", "2023-09-05", "草稿"));
+        articleList.add(new Article("医疗反腐绝非砍医护收入", "时事", "2024-07-31", "已发布"));
+        articleList.add(new Article("中国劈篮缘何一败涂地?", "篮球", "2024-07-31", "草稿"));
         articleList.add(
-                new Article("华山景区已受大风影响阵风达7-8级,未来24小时将持续", "旅游", "2023-09-07", "已发布"));
+                new Article("华山景区已受大风影响阵风达7-8级,未来24小时将持续", "旅游", "2024-07-31", "已发布"));
+        articleList.add(new Article("a1", "时事", "2024-09-05", "已发布"));
+        articleList.add(new Article("a2", "时事", "2024-09-05", "已发布"));
+        articleList.add(new Article("a3", "时事", "2024-09-05", "已发布"));
+        articleList.add(new Article("a4", "时事", "2024-09-05", "已发布"));
+        articleList.add(new Article("a5", "时事", "2024-09-05", "已发布"));
+        articleList.add(new Article("a6", "时事", "2024-09-05", "已发布"));
+        articleList.add(new Article("a7", "时事", "2024-09-05", "已发布"));
+        articleList.add(new Article("a8", "时事", "2024-09-05", "已发布"));
     }
     
     /**
@@ -55,14 +63,22 @@ public class ArticleController {
      */
     @RequestMapping("/search")
     public List<Article> search(@RequestBody Article article) {
+        String title = article.getTitle();
         String category = article.getCategory();
         String state = article.getState();
+        String time = article.getTime();
         List<Article> resultList = new ArrayList<>(articleList);
+        if (!StringUtils.isEmpty(title)) {
+            resultList = resultList.stream().filter(a -> a.getTitle().indexOf(title) > -1).collect(Collectors.toList());
+        }
         if (!StringUtils.isEmpty(category)) {
             resultList = resultList.stream().filter(a -> a.getCategory().equals(category)).collect(Collectors.toList());
         }
         if (!StringUtils.isEmpty(state)) {
             resultList = resultList.stream().filter(a -> a.getState().equals(state)).collect(Collectors.toList());
+        }
+        if (!StringUtils.isEmpty(time)) {
+            resultList = resultList.stream().filter(a -> a.getTime().equals(time)).collect(Collectors.toList());
         }
         /* articleList.stream().filter(a -> a.getCategory().equals(category) && a.getState().equals(state)).collect
         (Collectors.toList());*/
